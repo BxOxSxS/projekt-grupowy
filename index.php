4bb765826d6e0002d3313dbc037739d0fc4359c6
BxOxSxS <BxOxSxS@protonmail.com>
2023-10-27 11:23:48 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    require_once('vendor/autoload.php');
+
+    use Steampixel\Route;
+
+    Route::add('/', function() {
+        echo 'test';
+    });
+
+
+    Route::run('/bsadowski/projekt-grupowy')
+
+
+?>
