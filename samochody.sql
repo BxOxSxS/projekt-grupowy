09ed029dd6a4555aed66b265255621ea52c812fc
KruZ73 <100943604+KruZ73@users.noreply.github.com>
2023-10-27 11:37:36 +0200

    Baza danych - poprawiona

--- samochody.sql
+++ samochody.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 27 Paź 2023, 11:17
+-- Czas generowania: 27 Paź 2023, 11:36
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.0.19
 
@@ -32,7 +32,7 @@ CREATE TABLE `samochody` (
   `marka` varchar(50) DEFAULT NULL,
   `model` varchar(50) DEFAULT NULL,
   `rocznik` varchar(50) DEFAULT NULL,
-  `cena` varchar(8) DEFAULT NULL
+  `cena` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -40,26 +40,26 @@ CREATE TABLE `samochody` (
 --
 
 INSERT INTO `samochody` (`id`, `marka`, `model`, `rocznik`, `cena`) VALUES
-(1, 'Volkswagen', 'Eos', '2011', '30.000$'),
-(2, 'Hyundai', 'Azera', '2012', '250.000$'),
-(3, 'Porsche', 'Cayman', '2011', '200.000$'),
-(4, 'Audi', 'A6', '1999', '200.000$'),
-(5, 'Dodge', 'Magnum', '2006', '250.000$'),
-(6, 'BMW', 'M3', '1998', '100.000$'),
-(7, 'Chevrolet', 'Silverado 1500', '2010', '100.000$'),
-(8, 'Nissan', 'Maxima', '2005', '750.000$'),
-(9, 'Plymouth', 'Breeze', '1997', '30.000$'),
-(10, 'Honda', 'Element', '2005', ''),
-(11, 'Chevrolet', 'Silverado 1500', '2011', ''),
-(12, 'Acura', 'Legend', '1990', '250.000$'),
-(13, 'Volvo', 'XC90', '2003', '100.000$'),
-(14, 'Subaru', 'Impreza', '1997', '250.000$'),
-(15, 'Subaru', 'Alcyone SVX', '1992', '750.000$'),
-(16, 'Volkswagen', 'Passat', '2006', '100.000$'),
-(17, 'Chevrolet', 'Camaro', '1982', ''),
-(18, 'Nissan', 'Frontier', '2012', ''),
-(19, 'Audi', 'S8', '2001', '30.000$'),
-(20, 'Toyota', 'RAV4', '2004', '30.000$');
+(1, 'Saturn', 'Sky', '2008', 486312),
+(2, 'Volkswagen', 'Type 2', '1988', 756105),
+(3, 'Honda', 'Passport', '1995', 135924),
+(4, 'Hyundai', 'Sonata', '2008', 92222),
+(5, 'Cadillac', 'Eldorado', '1992', 806578),
+(6, 'GMC', 'Envoy XL', '2002', 55357),
+(7, 'Mitsubishi', 'Pajero', '1999', 255557),
+(8, 'Mitsubishi', 'Montero', '1992', 759407),
+(9, 'Acura', 'RSX', '2002', 927486),
+(10, 'Infiniti', 'IPL G', '2012', 844244),
+(11, 'Mercedes-Benz', 'SLR McLaren', '2008', 188874),
+(12, 'Mazda', 'Mazda5', '2008', 339706),
+(13, 'GMC', 'Yukon', '1993', 251296),
+(14, 'Dodge', 'Ram Van 2500', '1995', 614866),
+(15, 'Dodge', 'Nitro', '2010', 55781),
+(16, 'Ford', 'Escape', '2011', 146658),
+(17, 'Mercury', 'Grand Marquis', '1985', 82598),
+(18, 'GMC', 'Yukon', '2004', 137497),
+(19, 'Ford', 'F-Series', '2011', 831256),
+(20, 'Toyota', 'Prius c', '2012', 198069);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
