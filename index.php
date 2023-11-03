3749cddf21ec7a3ad73a0fa9ba9f8ea27be4eebe
B.O.S.S <BxOxSxS@protonmail.com>
2023-11-03 11:56:33 +0100

    Add simple GET method for all data

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
     $db = new mysqli("localhost", "root", "", "samochody");
 
     Route::add('/', function() {
-        echo 'test';
+        global $db;
+        $q = "SELECT * FROM samochody;";
+        $preparedQ = $db->prepare($q);
+        if (!$preparedQ->execute()) {
+            http_response_code(500);
+        } else {
+            $row = $preparedQ->get_result();
+
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode($row->fetch_all());
+        }
     });
 
 
